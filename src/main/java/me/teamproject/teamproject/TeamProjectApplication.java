20ceddc26b40d2ee5a343462487c8b016cbd27cc
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-08-21 16:35:25 +0900

    init: [김진호] Spring 초기 설정[MUSE-S-1]

---
+++ src/main/java/me/teamproject/teamproject/TeamProjectApplication.java
@@ -0,0 +1,11 @@
+package me.teamproject.teamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProjectApplication.class, args);
+    }
+}
