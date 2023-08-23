7026be5f8920e70aac520f0f36ed898fc9b13ddc
wlsdbwls <wlsdbwls614@gmail.com>
2023-08-23 16:41:58 +0900

    feat: [진유진] 사용자가 플레이리스트를 등록할 수 있습니다 [MUSE-S-3]

--- src/main/java/me/muse/CrezyBackend/domain/playlist/service/PlaylistService.java
+++ src/main/java/me/muse/CrezyBackend/domain/playlist/service/PlaylistService.java
@@ -1,12 +1,13 @@
 package me.muse.CrezyBackend.domain.playlist.service;
 
 import me.muse.CrezyBackend.domain.playlist.controller.form.PlaylistReadResponseForm;
+import me.muse.CrezyBackend.domain.playlist.controller.form.PlaylistRegisterRequestForm;
 import me.muse.CrezyBackend.domain.playlist.controller.form.PlaylistResponseForm;
 
 import java.util.List;
 
 public interface PlaylistService {
     List<PlaylistResponseForm> list();
-
     PlaylistReadResponseForm read(Long playlistId);
+    long register(PlaylistRegisterRequestForm requestForm);
 }
