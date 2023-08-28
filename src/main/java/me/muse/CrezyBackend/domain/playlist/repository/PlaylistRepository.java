88ef92e838cdd5cb79db1d6f2d8bc4ad67a2922c
wlsdbwls <wlsdbwls614@gmail.com>
2023-08-29 00:46:02 +0900

    feat: [진유진] 사용자는 자신이 등록한 플레이리스트 목록을 볼 수 있습니다 [MUSE-S-21]

--- src/main/java/me/muse/CrezyBackend/domain/playlist/repository/PlaylistRepository.java
+++ src/main/java/me/muse/CrezyBackend/domain/playlist/repository/PlaylistRepository.java
@@ -1,5 +1,6 @@
 package me.muse.CrezyBackend.domain.playlist.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import jakarta.transaction.Transactional;
 import me.muse.CrezyBackend.domain.playlist.entity.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
-    @Query("SELECT p FROM Playlist p JOIN FETCH p.account JOIN FETCH p.songlist LEFT JOIN FETCH p.likers")
+    @Query("SELECT p FROM Playlist p JOIN FETCH p.account")
     List<Playlist> findAll();
 
     @Query("SELECT DISTINCT p FROM Playlist p LEFT JOIN FETCH p.songlist LEFT JOIN FETCH p.account LEFT JOIN FETCH p.likers WHERE p.id = :id")
@@ -18,4 +19,8 @@ public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
     @Modifying
     @Query("DELETE FROM Playlist p WHERE p.account.accountId = :accountId")
     void deleteByAccountId(Long accountId);
+
+    // JOIN FETCH 부분 추후 수정
+    @Query("SELECT p FROM Playlist p JOIN FETCH p.account WHERE p.account.accountId = :accountId ")
+    List<Playlist> findByAccountId(Long accountId);
 }
