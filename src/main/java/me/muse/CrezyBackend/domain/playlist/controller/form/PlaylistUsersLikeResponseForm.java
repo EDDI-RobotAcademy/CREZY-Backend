a2715c8bd89acb5b268fd335d3a9a248dba51d32
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-08-29 12:24:40 +0900

    feat: [김진호] 사용자는 자신이 좋아요를 누른 플레이리스트 목록을 볼 수 있습니다 [MUSE-S-20]

---
+++ src/main/java/me/muse/CrezyBackend/domain/playlist/controller/form/PlaylistUsersLikeResponseForm.java
@@ -0,0 +1,19 @@
+package me.muse.CrezyBackend.domain.playlist.controller.form;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import me.muse.CrezyBackend.domain.song.entity.Song;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class PlaylistUsersLikeResponseForm {
+    final private Long playlistId;
+    final private String thumbnailName;
+    final private String accountWriter;
+    final private List<Song> songlist;
+    final private int likers;
+
+}
+
