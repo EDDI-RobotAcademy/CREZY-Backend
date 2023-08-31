39bc0217aa9a032b88b32d857958d51712b5075a
wlsdbwls <wlsdbwls614@gmail.com>
2023-08-31 12:45:00 +0900

    update: [진유진] 사용자 정보 - 프로필 이미지 포함하여 반환하도록 변경 [MUSE-S-23]

--- src/main/java/me/muse/CrezyBackend/domain/account/controller/form/AccountInfoResponseForm.java
+++ src/main/java/me/muse/CrezyBackend/domain/account/controller/form/AccountInfoResponseForm.java
@@ -7,10 +7,18 @@ public class AccountInfoResponseForm {
     private String nickname;
     private int myPlaylistCount;
     private int myLikedPlaylistCount;
+    private String profileImageName;
 
     public AccountInfoResponseForm(String nickname, int myPlaylistCount, int myLikedPlaylistCount) {
         this.nickname = nickname;
         this.myPlaylistCount = myPlaylistCount;
         this.myLikedPlaylistCount = myLikedPlaylistCount;
+    }
+
+    public AccountInfoResponseForm(String nickname, int myPlaylistCount, int myLikedPlaylistCount, String profileImageName) {
+        this.nickname = nickname;
+        this.myPlaylistCount = myPlaylistCount;
+        this.myLikedPlaylistCount = myLikedPlaylistCount;
+        this.profileImageName = profileImageName;
     }
 }
