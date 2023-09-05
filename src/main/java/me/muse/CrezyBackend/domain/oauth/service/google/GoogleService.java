0fe2ae27fa91fcd304bffc31bf99eb62cb1ad49f
YeoulCho <cyu5733@gmail.com>
2023-09-05 15:30:10 +0900

    update: [조여울] Google OAuth RefreshToken 발급 [MUSE-S-6]

--- src/main/java/me/muse/CrezyBackend/domain/oauth/service/google/GoogleService.java
+++ src/main/java/me/muse/CrezyBackend/domain/oauth/service/google/GoogleService.java
@@ -3,11 +3,13 @@ package me.muse.CrezyBackend.domain.oauth.service.google;
 import me.muse.CrezyBackend.domain.account.entity.Account;
 import me.muse.CrezyBackend.domain.oauth.controller.form.LoginRequestForm;
 import me.muse.CrezyBackend.domain.oauth.controller.form.LoginResponseForm;
+import me.muse.CrezyBackend.domain.oauth.dto.GoogleOAuthToken;
 import org.springframework.http.ResponseEntity;
 
 public interface GoogleService {
     String googleLoginAddress();
-    LoginResponseForm getAccount(String code);
-    LoginResponseForm getNewAccount(String code, LoginRequestForm requestForm);
+    LoginResponseForm getAccount();
+    LoginResponseForm getNewAccount(LoginRequestForm requestForm);
     boolean checkDuplicateAccount(String code);
+
 }
