6ab9f39809da2bb138edb2db45ae420b8f49b1e4
namhyunki <adamwor0505@gmail.com>
2023-09-06 11:14:13 +0900

    fix: [남현기] 배포 주소 Cors 변경 [MUSE-I-9]

--- src/main/java/me/muse/CrezyBackend/config/CorsConfig.java
+++ src/main/java/me/muse/CrezyBackend/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:8080",
                         "http://localhost:8080",
-                        "http://15.164.180.204")
+                        "http://3.34.151.225")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
