da3defe0aff30521f98c2fe1bbb6c4f547a87ae5
YeoulCho <cyu5733@gmail.com>
2023-09-11 11:53:57 +0900

    feat: [조여울] 관리자는 관리자 페이지에서 로그인 할 수 있습니다 [MUSE-S-38]

---
+++ src/main/java/me/muse/CrezyBackend/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package me.muse.CrezyBackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        return http.build();
+    }
+}
