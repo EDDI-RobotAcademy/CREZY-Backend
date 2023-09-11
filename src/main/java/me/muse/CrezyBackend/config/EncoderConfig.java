a198f3a29177604f51af4da739f239127ab62adf
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-09-11 12:53:16 +0900

    feat: [남현기] DB Initialize 할 때 관리자 계정 정보를 저장합니다. [MUSE-S-39]

---
+++ src/main/java/me/muse/CrezyBackend/config/EncoderConfig.java
@@ -0,0 +1,13 @@
+package me.muse.CrezyBackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class EncoderConfig {
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/me/muse/CrezyBackend/domain/account/repository/ProfileRepository.java
+++ src/main/java/me/muse/CrezyBackend/domain/account/repository/ProfileRepository.java
@@ -1,12 +1,10 @@
 package me.muse.CrezyBackend.domain.account.repository;
 
-import me.muse.CrezyBackend.domain.account.entity.Account;
-import me.muse.CrezyBackend.domain.account.entity.AccountLoginType;
-import me.muse.CrezyBackend.domain.account.entity.LoginType;
-import me.muse.CrezyBackend.domain.account.entity.Profile;
+import me.muse.CrezyBackend.domain.account.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProfileRepository extends JpaRepository<Profile,Long> {
@@ -15,4 +13,5 @@ public interface ProfileRepository extends JpaRepository<Profile,Long> {
     Optional<Profile> findByEmailAndAccount_LoginType(String email, AccountLoginType loginType);
     Optional<Profile> findByNickname(String nickname);
     Optional<Profile> findByAccount(Account account);
+    List<Profile> findByAccount_RoleType(AccountRoleType roleType);
 }
