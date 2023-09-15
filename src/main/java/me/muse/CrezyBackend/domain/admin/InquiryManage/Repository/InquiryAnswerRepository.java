f8f7408c9ace771cc397e26b76c819a9b9d440f5
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-09-15 16:55:51 +0900

    feat: [남현기] 관리자는 문의 내역에 답변을 할 수 있습니다. [MUSE-S-59]

---
+++ src/main/java/me/muse/CrezyBackend/domain/admin/InquiryManage/Repository/InquiryAnswerRepository.java
@@ -0,0 +1,7 @@
+package me.muse.CrezyBackend.domain.admin.InquiryManage.Repository;
+
+import me.muse.CrezyBackend.domain.admin.InquiryManage.Entity.InquiryAnswer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InquiryAnswerRepository extends JpaRepository<InquiryAnswer, Long> {
+}
---
+++ src/main/java/me/muse/CrezyBackend/domain/admin/InquiryManage/controller/form/AdminInquiryAnswerRegisterForm.java
@@ -0,0 +1,9 @@
+package me.muse.CrezyBackend.domain.admin.InquiryManage.controller.form;
+
+import lombok.Getter;
+
+@Getter
+public class AdminInquiryAnswerRegisterForm {
+    private String inquiryAnswer;
+    private Long inquiryId;
+}
