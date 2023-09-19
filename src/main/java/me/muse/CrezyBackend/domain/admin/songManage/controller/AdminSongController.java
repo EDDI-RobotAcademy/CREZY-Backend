3c1dda366776f5311442be09aad0f88de46a8961
YeoulCho <cyu5733@gmail.com>
2023-09-19 09:52:13 +0900

    add: [조여울] 노래 statusType 추가 [MUSE-S-69]

--- src/main/java/me/muse/CrezyBackend/domain/admin/songManage/controller/AdminSongController.java
+++ src/main/java/me/muse/CrezyBackend/domain/admin/songManage/controller/AdminSongController.java
@@ -2,7 +2,7 @@ package me.muse.CrezyBackend.domain.admin.songManage.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import me.muse.CrezyBackend.domain.admin.playlistManage.controller.form.AdminPlaylistSongDetailReadResponseForm;
+import me.muse.CrezyBackend.domain.admin.songManage.controller.form.AdminSongDetailReadResponseForm;
 import me.muse.CrezyBackend.domain.admin.songManage.controller.form.AdminSongListRequestForm;
 import me.muse.CrezyBackend.domain.admin.songManage.controller.form.AdminSongListResponseForm;
 import me.muse.CrezyBackend.domain.admin.songManage.controller.form.AdminSongModifyLyricsRequestForm;
@@ -30,7 +30,7 @@ public class AdminSongController {
     }
 
     @GetMapping("/read-song")
-    public AdminPlaylistSongDetailReadResponseForm readSongDetail(@RequestHeader HttpHeaders headers, @RequestParam("songId") Long SongId) {
+    public AdminSongDetailReadResponseForm readSongDetail(@RequestHeader HttpHeaders headers, @RequestParam("songId") Long SongId) {
         log.info("readSongDetail()");
         return adminService.readSongDetail(headers, SongId);
     }
--- src/main/java/me/muse/CrezyBackend/domain/admin/songManage/service/AdminSongService.java
+++ src/main/java/me/muse/CrezyBackend/domain/admin/songManage/service/AdminSongService.java
@@ -1,6 +1,6 @@
 package me.muse.CrezyBackend.domain.admin.songManage.service;
 
-import me.muse.CrezyBackend.domain.admin.playlistManage.controller.form.AdminPlaylistSongDetailReadResponseForm;
+import me.muse.CrezyBackend.domain.admin.songManage.controller.form.AdminSongDetailReadResponseForm;
 import me.muse.CrezyBackend.domain.admin.songManage.controller.form.AdminSongListRequestForm;
 import me.muse.CrezyBackend.domain.admin.songManage.controller.form.AdminSongListResponseForm;
 import me.muse.CrezyBackend.domain.admin.songManage.controller.form.AdminSongModifyLyricsRequestForm;
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 
 public interface AdminSongService {
-    AdminPlaylistSongDetailReadResponseForm readSongDetail(HttpHeaders headers, Long songId);
+    AdminSongDetailReadResponseForm readSongDetail(HttpHeaders headers, Long songId);
     Boolean registerSongStatusBlock(Long songId, HttpHeaders headers);
     Boolean registerSongStatusOpen(Long songId, HttpHeaders headers);
     Page<AdminSongListResponseForm> list(HttpHeaders headers, AdminSongListRequestForm requestForm);
